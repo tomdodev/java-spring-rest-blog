b694922ec3caa089f6cb3521157f1127fd8f091e
Tom D <tomdodev@gmail.com>
2022-03-27 11:11:58 -0400

    step4

--- src/main/java/com/pluralsight/blog/model/Author.java
+++ src/main/java/com/pluralsight/blog/model/Author.java
@@ -17,6 +17,9 @@ public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Version private Long version;
+
     private String firstname;
     private String lastname;
     @JsonIgnore
